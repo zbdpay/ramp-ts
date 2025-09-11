54132b4ad2ec69e30c7cddb266ae8657845a080d
Saul Freeberty <freeberty@zbdpay.com>
2025-09-11 19:42:44 +0100

    export refreshAccessToken

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
 export { ZBDRamp, createRamp } from './ramp.js';
 export * from './types.js';
 export * from './utils.js';
-export { initRampSession } from './session.js';
+export { initRampSession, refreshAccessToken } from './session.js';
 
 export { createRamp as default } from './ramp.js';
